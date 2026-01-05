ff949ff93dd0d1bde2fa505253547fc5242f33b6
Drlesh2024 <michael.laczkowski@googlemail.com>
2026-01-05 01:23:18 +0100

    Create budibase.config.js

---
+++ budibase.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  type: "component",
+  name: "Resource Timeline",
+  icon: "calendar",
+  description: "Timeline calendar with resources (workers) on the left and assignments as bars"
+};
