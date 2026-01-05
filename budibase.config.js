c95d7883db80ff740a97cde24cd717a9512f5feb
Drlesh2024 <michael.laczkowski@googlemail.com>
2026-01-05 01:35:37 +0100

    Update and rename budibase.config.js to schema.json

--- budibase.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  type: "component",
-  name: "Resource Timeline",
-  icon: "calendar",
-  description: "Timeline calendar with resources (workers) on the left and assignments as bars"
-};
